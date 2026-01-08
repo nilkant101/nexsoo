eea307dfee7d22d3e347e50442391e7a3e31c38c
nilkant101 <nilwarle108@gmail.com>
2026-01-08 16:18:14 +0530

    shifted tailwind v3

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         dark: "#0B0F0E",
+//         accent: "#2ED3A7",
+//         accentHover: "#22C55E",
+//         muted: "#9CA3AF",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         dark: "var(--color-dark)",
+//         accent: "var(--color-accent)",
+//         accentHover: "var(--color-accent-hover)",
+//         muted: "var(--color-muted)",
+//       },
+//     },
+//   },
+// };
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
